7499c98ded5866ef0c35ca3b3961307d1b6daa04
youssef <55909614+LoneWolf-84@users.noreply.github.com>
2021-03-16 18:02:28 +0200

    V1.0

---
+++ app/src/main/java/com/example/result_faculty/DashBoard_activity.java
@@ -0,0 +1,14 @@
+package com.example.result_faculty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashBoard_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+    }
+}
--- app/src/main/java/com/example/result_faculty/MainActivity.java
+++ app/src/main/java/com/example/result_faculty/MainActivity.java
@@ -1,22 +1,75 @@
 package com.example.result_faculty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnlogin;
+    FloatingActionButton btnlogin;
     String mail;
+    EditText ed;
+    EditText ed2;
+    String password;
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnlogin = findViewById(R.id.btnlogin);
+        ed = findViewById(R.id.in);
+        ed2 = findViewById(R.id.pass);
+        mAuth = FirebaseAuth.getInstance();
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vi) {
+                mail = ed.getText().toString().trim();
+                password = ed2.getText().toString().trim();
+
+                if (TextUtils.isEmpty(mail)) {
+                    ed.setError("Enter The E-mail First");
+                    return;
+                }  else if (TextUtils.isEmpty(password)) {
+                    ed2.setError("Please Enter Your Password ");
+                    return;
+                } else if (ed2.length() != 14) {
+                    ed2.setError("Must Be 14 Numbers");
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Snackbar mySnackbar = Snackbar.make(findViewById(R.id.cc),
+                                    "Signed In", Snackbar.LENGTH_SHORT);
+                            mySnackbar.show();
+                            Intent i = new Intent(MainActivity.this,DashBoard_activity.class);
+                            startActivity(i);
+                        }
+                        else {
+
+                            Snackbar mySnackbar = Snackbar.make(findViewById(R.id.cc),
+                                    "Wrong E-Mail Or Password", Snackbar.LENGTH_SHORT);
+                            mySnackbar.show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
 }
