7df3e78c80365631890d6824332603c633abc952
youssef <yoer766@gmail.com>
2021-03-16 18:07:27 +0200

    V1.4

--- app/src/main/java/com/example/result_faculty/MainActivity.java
+++ app/src/main/java/com/example/result_faculty/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
