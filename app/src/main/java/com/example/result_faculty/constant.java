4f98df9e2185ff7196ea93d03d54bd605c97d797
youssef <yoer766@gmail.com>
2021-03-18 11:56:49 +0200

    V1.6.6

--- app/src/main/java/com/example/result_faculty/constant.java
+++ app/src/main/java/com/example/result_faculty/constant.java
@@ -3,12 +3,12 @@ package com.example.result_faculty;
 public class constant {
     static final String subject1= "business";
     static final String subject2= "discrete";
-    static final String subject3= "tecnical writing";
-    static final String subject4= "probability";
-    static final String subject5= "pm";
-    static final String subject6= "oop";
-    static final String subject7= "is";
-    static final String subject8= "ethics";
+    static final String subject3= "ethics";
+    static final String subject4= "is";
+    static final String subject5= "oop";
+    static final String subject6= "pm";
+    static final String subject7= "probability";
+    static final String subject8= "tecnical writing";
 
 
     static final String subject_degree= "degree";
